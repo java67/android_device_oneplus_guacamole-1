b1d65907a664bfd465fb5332709ac79ec08ac13d
Justin Crawford <Justin@stacksmash.net>
2020-09-21 12:54:01 -0700

    guacamole: rebrand yaap -> aosp
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_guacamole.mk
+    $(LOCAL_DIR)/aosp_guacamole.mk
 
 COMMON_LUNCH_CHOICES := \
-	yaap_guacamole-user \
-	yaap_guacamole-userdebug \
-	yaap_guacamole-eng
+	aosp_guacamole-user \
+	aosp_guacamole-userdebug \
+	aosp_guacamole-eng
